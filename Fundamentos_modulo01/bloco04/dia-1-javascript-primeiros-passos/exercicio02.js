4a7e0f524bce2897cbbaa3aa631ebf2974d9f1b2
Siigmaf <faby.tbe@gmail.com>
2022-06-21 18:57:53 -0300

    adiciona exercicios 01 02 03

---
+++ Fundamentos_modulo01/bloco04/dia-1-javascript-primeiros-passos/exercicio02.js
@@ -0,0 +1,11 @@
+//02-Faça um programa que retorne o maior de dois números. 
+//Defina no começo do programa duas constantes com os valores que serão comparados.
+
+const numero01=5;
+const numero04=7;
+
+if(numero01>numero04){
+    console.log(numero01);
+}else{
+    console.log(numero04);
+}
---
+++ Fundamentos_modulo01/bloco04/dia-1-javascript-primeiros-passos/exercicios03.js
@@ -0,0 +1,11 @@
+//3-Faça um programa que retorne o maior de três números. 
+//Defina no começo do programa três constantes com os valores que serão comparados.
+
+//4-Faça um programa que, dado um valor definido numa constante, retorne "positive" 
+//se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+const numero02=10;
+const numero03=6;
+const numero05=9;
+
+if(numero02>numero03)
