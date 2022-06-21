f4315ec33e0e2f24778a2a34fe0cfa0dca66d717
Siigmaf <faby.tbe@gmail.com>
2022-06-21 19:04:54 -0300

    conclusão exercicio 03

--- Fundamentos_modulo01/bloco04/dia-1-javascript-primeiros-passos/exercicio03.js
+++ Fundamentos_modulo01/bloco04/dia-1-javascript-primeiros-passos/exercicio03.js
@@ -1,11 +1,14 @@
 //3-Faça um programa que retorne o maior de três números. 
 //Defina no começo do programa três constantes com os valores que serão comparados.
 
-//4-Faça um programa que, dado um valor definido numa constante, retorne "positive" 
-//se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
-
 const numero02=10;
 const numero03=6;
 const numero05=9;
 
-if(numero02>numero03)
+if(numero02>numero03&&numero05){
+    console.log(numero02);
+}else if (numero03>numero02&&numero05){
+    console.log(numero03);
+}else{
+    console.log(numero05);
+}
