ebb3e3dcbd2ee712adbc9d879fc97de55a1d7b2d
Siigmaf <faby.tbe@gmail.com>
2022-06-23 10:49:23 -0300

    conclui exercicio 08

---
+++ Fundamentos_modulo01/bloco04/dia-2-javascript-array-e-loop-for/exercicio08.js
@@ -0,0 +1,10 @@
+//Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+
+let listaNumbers=[];
+
+for (let index = 1; index <= 25; index+=1) {
+    listaNumbers.push(index);
+    
+}
+console.log(listaNumbers);
+
